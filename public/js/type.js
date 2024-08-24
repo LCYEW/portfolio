d95d9b0de5d3d353fa7527fc8a2303931c34626c
LCYEW <167974549+LCYEW@users.noreply.github.com>
2024-08-24 18:43:07 +0800

    uploaded index and projects page

---
+++ public/js/type.js
@@ -0,0 +1,7 @@
+var typed = new Typed('#development', {
+    strings: ['Web Development', 'App Development', 'Machine Learning', 'Computer Vision'],
+    typeSpeed: 50,
+    backSpeed:50, 
+    smartBackspace: true,
+    loop: true
+  });
